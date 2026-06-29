ded9cf7a83230e3dfb1e0c0bd09afdf4cbb2aa3f
srujana-maddula <85280184+srujana-maddula@users.noreply.github.com>
2026-06-29 17:17:40 +0530

    Update discount.py

--- Fix-4/discount.py
+++ Fix-4/discount.py
@@ -1,10 +1,4 @@
-from catalog import BASE_PRICES
-
 def get_discount(price: float) -> float:
     if price > 500:
         return price * 0.10
     return price * 0.05
-
-def get_discounted_catalog() -> dict:
-    products = list(BASE_PRICES.keys())
-    return {p: BASE_PRICES[p] - get_discount(BASE_PRICES[p]) for p in products}
