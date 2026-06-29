9adfe31de8e885d9eabb676da01b4d1df6dc078a
srujana-maddula <85280184+srujana-maddula@users.noreply.github.com>
2026-06-29 17:20:44 +0530

    Delete basic-code-example/product.py

--- basic-code-example/product.py
+++
@@ -1,9 +0,0 @@
-from order import Order
-
-class Product:
-    def __init__(self, name: str, price: float):
-        self.name = name
-        self.price = price
-
-    def is_in_order(self, order: Order) -> bool:
-        return order.product.name == self.name
