86515895b40a2d7a95e9a862c2538404304d8aba
srujana-maddula <85280184+srujana-maddula@users.noreply.github.com>
2026-06-29 15:50:09 +0530

    pricing-fix1

---
+++ Fix-1/pricing.py
@@ -0,0 +1,11 @@
+from discount import get_discount
+
+def get_base_price(product: str) -> float:
+    base_prices = {"laptop": 1000.0, "phone": 600.0, "tablet": 400.0}
+    return base_prices.get(product, 0.0)
+
+def get_final_price(product: str, quantity: int) -> float:
+    price = get_base_price(product) * quantity
+    return price - get_discount(price)
+
+print(get_final_price("laptop", 3))
