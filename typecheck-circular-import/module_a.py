2cb94a2d5e69c2ab5ac1336626bb1d9939e08fd1
srujana-maddula <85280184+srujana-maddula@users.noreply.github.com>
2026-06-29 17:21:15 +0530

    Delete typecheck-circular-import directory

--- typecheck-circular-import/module_a.py
+++
@@ -1,5 +0,0 @@
-from module_b import B
-
-class A:
-    def process(self, item: B) -> None:
-        print(item)
--- typecheck-circular-import/module_b.py
+++
@@ -1,5 +0,0 @@
-from module_a import A
-
-class B:
-    def process(self, item: A) -> None:
-        print(item)
