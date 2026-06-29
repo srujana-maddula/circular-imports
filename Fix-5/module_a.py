bdc2b8f2bc8d8a7d69771fe734a463247d6d9602
srujana-maddula <85280184+srujana-maddula@users.noreply.github.com>
2026-06-29 17:21:00 +0530

    Delete Fix-5 directory

--- Fix-5/module_a.py
+++
@@ -1,9 +0,0 @@
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from module_b import B
-
-
-class A:
-    def process(self, item: "B") -> None:
-        print(item)
--- Fix-5/module_b.py
+++
@@ -1,9 +0,0 @@
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from module_a import A
-
-
-class B:
-    def process(self, item: "A") -> None:
-        print(item)
